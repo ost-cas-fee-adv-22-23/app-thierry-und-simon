6c13a2bcd1d1c84b031579bb2841e80a7fc6055d
eoss-sh <sh@eoss.ch>
2023-03-21 22:41:13 +0100

    added new Navibutton component, useSession and dummy styling for profile image

--- components/layout/navigation.tsx
+++ components/layout/navigation.tsx
@@ -1,13 +1,33 @@
+import { useSession } from 'next-auth/react'
+import { IconType, NaviButton } from '@smartive-education/thierry-simon-mumble'
+
 export const Navigation = () => {
+  const { data: session } = useSession()
+  console.log(session)
   return (
     <header className="h-20 bg-violet-600">
       <div className="flex items-center justify-between h-full max-w-3xl mx-auto px-10 color text-white">
         <h1>Mumble</h1>
         <nav>
-          <ul className="flex gap-6">
-            <li>Profil</li>
-            <li>Settings</li>
-            <li>Logout</li>
+          <ul className="flex items-center gap-6">
+            {session && (
+              <>
+                <li>
+                  <div className="rounded-full bg-purple-700 h-10 w-10"></div>
+                </li>
+                <li>
+                  <NaviButton text="Settings" icon={IconType.profile} />
+                </li>
+                <li>
+                  <NaviButton text="Logout" icon={IconType.logout} />
+                </li>
+              </>
+            )}
+            {!session && (
+              <li>
+                <NaviButton text="Login" icon={IconType.mumble} />
+              </li>
+            )}
           </ul>
         </nav>
       </div>
