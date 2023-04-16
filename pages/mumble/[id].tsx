330e32a61fb36edf7fa489b0e321b3d4f39eecd8
Thierry Seiler <>
2023-04-16 10:52:36 +0200

    tried with simpler approach (not using hooks) and fallback

--- pages/mumble/[id].tsx
+++ pages/mumble/[id].tsx
@@ -13,13 +13,31 @@ import { getToken } from 'next-auth/jwt'
 
 type Props = {
   mumbleId: string
+  fallback: any
 }
 
 export default function MumblePage({
   mumbleId,
   fallback
 }: Props): InferGetServerSidePropsType<typeof getServerSideProps> {
-  const { data: mumble, error, isLoading } = useSingleMumblesWithUser(mumbleId)
+  const { data: session, status }: any = useSession()
+
+  // const {
+  //   data: mumble,
+  //   error,
+  //   isLoading
+  // } = useSingleMumblesWithUser(mumbleId, fallback)
+
+  const {
+    data: mumble,
+    isLoading,
+    error
+  } = useSWR(
+    [mumbleId, session?.accessToken],
+    ([mumbleId, accessToken]) =>
+      fetchSingleMumbleWithUser(mumbleId, accessToken),
+    { fallback }
+  )
 
   if (mumble) {
     console.log(mumble)
@@ -32,6 +50,7 @@ export default function MumblePage({
       {isLoading && <p>Is Loading</p>}
       {mumble && <MumbleCard mumble={mumble} />}
       <WritePost />
+
       {/* {responses.length > 0 &&
         responses.map((response, index) => (
           <MumbleCard mumble={response} key={`mumblereponse-${index}`} />
@@ -48,18 +67,22 @@ export const getServerSideProps: GetServerSideProps = async ({
   const mumbleId = query.id
 
   const singleMumbleWithUser = await fetchSingleMumbleWithUser(
-    mumbleId as string,
-    token?.accessToken as string
+    mumbleId,
+    token?.accessToken
   )
 
   return {
     props: {
       mumbleId,
       fallback: {
-        [unstable_serialize(() =>
-          getKey(mumbleId as string, token?.accessToken as string)
-        )]: singleMumbleWithUser
+        [unstable_serialize([mumbleId, token?.accessToken])]:
+          singleMumbleWithUser
       }
+      // fallback: {
+      //   [unstable_serialize(() =>
+      //     getKey(mumbleId as string, token?.accessToken)
+      //   )]: singleMumbleWithUser
+      // }
     }
   }
 }
