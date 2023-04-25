bcbda972125ed05540fd6935701603190e8ae970
Thierry Seiler <>
2023-04-25 10:45:22 +0200

    added nprogress bar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,13 +4,35 @@ import '../styles/globals.css'
 import { Layout } from '../components/layout/layout'
 import { usePreserveScroll } from '../hooks/usePreserveScroll'
 import Head from 'next/head'
+import '../styles/nprogress.css'
+import NProgress from 'nprogress'
+import { useEffect } from 'react'
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
+
   ...appProps
 }: AppProps) {
   usePreserveScroll()
+
+  NProgress.configure({ showSpinner: false })
+  useEffect(() => {
+    const handleRouteStart = () => NProgress.start()
+    const handleRouteDone = () => NProgress.done()
+
+    appProps.router.events.on('routeChangeStart', handleRouteStart)
+    appProps.router.events.on('routeChangeComplete', handleRouteDone)
+    appProps.router.events.on('routeChangeError', handleRouteDone)
+
+    return () => {
+      // Make sure to remove the event handler on unmount!
+      appProps.router.events.off('routeChangeStart', handleRouteStart)
+      appProps.router.events.off('routeChangeComplete', handleRouteDone)
+      appProps.router.events.off('routeChangeError', handleRouteDone)
+    }
+  }, [appProps.router])
+
   const getLayout = () => {
     if ([`/login`, `/register`].includes(appProps.router.pathname)) {
       return <Component {...pageProps} />
