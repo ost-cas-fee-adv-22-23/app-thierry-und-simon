c603af8442f3c388a2b9e6cdb51dd8bddc46bb1e
eoss-sh <sh@eoss.ch>
2023-07-04 22:59:56 +0200

    update terraform user

--- terraform/cloud-run.tf
+++ terraform/cloud-run.tf
@@ -1,21 +1,3 @@
-resource "google_service_account" "cloud-runner" {
-  account_id   = "cloud-runner"
-  display_name = "Google Cloud Run"
-  description  = "Account to deploy applications to google cloud run."
-}
-
-resource "google_project_iam_member" "cloud-runner" {
-  for_each = toset([
-    "roles/run.serviceAgent",
-    "roles/viewer",
-    "roles/storage.objectViewer",
-    "roles/run.admin",
-    "roles/cloudsql.client"
-  ])
-  role    = each.key
-  member  = "serviceAccount:${google_service_account.cloud-runner.email}"
-  project = data.google_project.project.id
-}
 resource "google_project_iam_member" "cloud-runner-svc" {
   role    = "roles/run.serviceAgent"
   member  = "serviceAccount:service-${data.google_project.project.number}@serverless-robot-prod.iam.gserviceaccount.com"
